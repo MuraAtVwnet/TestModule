70a6120f95f6e1d4136a56a3ddc7efe79cfa41f2
Syuichi Murashima <mura@vwnet.jp>
2024-10-16 05:42:00 +0900

    Space to TAB

--- uninstall.ps1
+++ uninstall.ps1
@@ -9,19 +9,19 @@ $RemovePath = Join-Path $ModulePath $ModuleName
 
 # ディレクトリ削除
 if( Test-Path $RemovePath ){
-    Remove-Item $RemovePath -Force -Recurse
+	Remove-Item $RemovePath -Force -Recurse
 }
 
 # オンラインインインストール対応
 $UnInstaller = "UnInstall" + $ModuleName + ".ps1"
 if( Test-Path ~/$UnInstaller ){
-    Remove-Item ~/$UnInstaller
+	Remove-Item ~/$UnInstaller
 }
 
 # バージョン管理ファイル削除
 $Vertion = "Vertion" + $ModuleName + ".txt"
 if( Test-Path ~/$Vertion ){
-    Remove-Item ~/$Vertion
+	Remove-Item ~/$Vertion
 }
 
 
